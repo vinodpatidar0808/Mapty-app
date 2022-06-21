89c33584c75e25772536f67d34a3a487c407c291
vinod_0808 <vinodpatidar0808@gmail.com>
2022-06-21 23:42:38 +0530

    managing workout data: creating classes

--- script.js
+++ script.js
@@ -11,6 +11,53 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+    date = new Date();
+    // in real life we generally use to create ids , its not a good idea to create our own ids
+    id = (Date.now() + '').slice(-10);
+    constructor(coords, distance, duration) {
+        this.coords = coords; // array of latitude and longitude --> [lat, long]
+        this.distance = distance; // in km
+        this.duration = duration; // in minutes
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace() {
+        // minutes /km
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = this.elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        // km/h
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+// const run = new Running([59, -12], 5.2, 24, 170);
+// const cyc = new Cycling([39, -12], 26, 90, 560);
+
+// console.log(run);
+// console.log(cyc);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Application architecture
 class App {
     #map;
     #mapEvent;
@@ -50,7 +97,6 @@ class App {
         // this on method is from leaflet library not from js
         // if you attach event handler to entire map element you won't be able to know where user clicked and so you will be unable to access location coordinates
         this.#map.on('click', this._showForm.bind(this));
-
     }
 
     _showForm(mapE) {
@@ -58,7 +104,7 @@ class App {
         form.classList.remove('hidden');
         inputDistance.focus();
     }
-    
+
     _toggleElevationField() {
         inputElevation
             .closest('.form__row')
@@ -102,18 +148,5 @@ const app = new App();
 // to avoid this we have a method in class that gets executed immediately when object is created, constructor method.
 // app._getPosition()
 
-class Workout {
-    constructor(id, distance, duration, coords) {
-        this.distance = distance;
-        this.duration = duration;
-        this.coords = coords;
-    }
-}
-
-class Running extends Workout {
-    constructor() {
-        super();
-    }
-}
 // geolocation API : browser API
 // getCurrentPosition(success callback fxn, error callback fxn)]
